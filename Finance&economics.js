215b5eb3f930d298f3a7b51000b2a346c133a707
weigw <weigw001@gmail.com>
2021-12-19 15:06:36 +0800

    Finance&economics.js

---
+++ Finance&economics.js
@@ -0,0 +1,36 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const moment = require('moment');
+const fse = require('fs-extra');
+
+(async () => {
+  let data = [];
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: "./data",
+  });
+  const page = await browser.newPage();
+  for (let pg = 1; pg <= 10; pg++) {
+    await page.goto(
+      `https://www.economist.com/finance-and-economics?page=${pg}`
+    );
+
+    let listSelector = [".teaser__subheadline", ".teaser__headline.teaser__headline--sc3", ".teaser__description.teaser__description--sc2"];
+    for (selector of listSelector) {
+      await page.waitForSelector(selector);
+      let titles = await page.$$eval(
+        selector,
+        (links) => links.map((x) => x.innerText)
+      );
+      if (data.length == 0) {
+        data = titles;
+      }
+      else {
+        data = titles.map((e, i) => data[i] + '\n' + e);
+      }
+    }
+  }
+  browser.close();
+  await fse.outputFile(moment().format('YYYY-MM-DD') + '/Finance&economics.txt', data.join('\n\n'));
+})();
+
