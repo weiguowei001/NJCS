ee1e5094b1a06ec80b151ff1f8927daab261de98
weigw <15851830359@163.com>
2021-12-22 00:27:29 +0800

    automatic script

---
+++ autoscript.bat
@@ -0,0 +1,11 @@
+node classical-music-and-opera.js
+node finance-and-economics.js
+node science-and-technology.js
+node theguardian-and-books.js
+node classical-music-and-opera.js
+node theguardian-and-cluture.js
+python .\ciyun\traversedirectory.py
+git add *
+git commit -m "automatic script"
+git push
+pause
