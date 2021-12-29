711752de8c7530e6cab90d60d293606f169d700d
weigw <15851830359@163.com>
2021-12-30 01:59:53 +0800

    automatic script

--- ciyun/traversedirectory.py
+++ ciyun/traversedirectory.py
@@ -2,12 +2,16 @@
 
 import os
 import simple
+from pathlib import Path
 
 # traverse root directory, and list directories as dirs and files as files
 for root, dirs, files in os.walk("."):
     # path = root.split(os.sep)
-    # print((len(path) - 1) * '---', os.path.basename(root))
+    # print((len(path) - 1) * '---', os.path.basenamos.pathe(root))
     for file in files:
         # print(len(path) * '---', file)
-        if (root.find("2021") != -1 and file.endswith(".txt")):
-            simple.simplewordcloud(os.path.join(root, file))
+        txt = Path(os.path.join(root, file))
+        png = Path(os.path.join(root, file)).with_suffix('.png')
+        if (root.find("2021") != -1 and txt.suffix == ".txt"):
+            if (not png.exists() or os.path.getmtime(txt) > os.path.getmtime(png)):
+                simple.simplewordcloud(txt)
